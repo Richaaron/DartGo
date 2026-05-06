7b4dc019e601ec096a51c805cc51e03bc95f4662
Developer <dev@example.com>
2026-05-06 23:19:50 +0100

    fix: remove erroneous @fontsource imports

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,6 @@ import { AuthProvider } from './context/AuthContext'
 import { DEFAULT_SUBJECTS } from './types'
 import { ErrorBoundary } from './components/ErrorBoundary'
 import { ToastProvider } from './utils/toast'
-import '@fontsource/inter/400.css'
-import '@fontsource/inter/500.css'
-import '@fontsource/inter/600.css'
-import '@fontsource/inter/700.css'
-import '@fontsource/inter/900.css'
 import './index.css'
 
 // Suppress harmless Recharts warnings about width/height during Framer Motion animations
